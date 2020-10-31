ce0806ef621ae5aed9c686452d27486980a99a4d
Anurag Singh <anuragrs@gmail.com>
2020-10-31 08:22:40 +0000

    herring integration refactor - can swtich on herring use via USE_HERRING_ALL_REDUCE env var carl loss fix - needs more tests best hp for non herring without carl - consistent 16 epoch convergence

---
+++ NVIDIA/benchmarks/maskrcnn/implementations/pytorch/launch_cosine_novo_giou_ls_512_herring.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# this script has novograd + herring
+
+#source /shared/herring_mzanur/bin/activate
+
+BASE_LR=0.05
+MAX_ITER=4000
+WARMUP_FACTOR=0.000133
+WARMUP_ITERS=1000
+TRAIN_IMS_PER_BATCH=512
+TEST_IMS_PER_BATCH=256
+WEIGHT_DECAY=1.25e-3
+NSOCKETS_PER_NODE=2
+NCORES_PER_SOCKET=24
+NPROC_PER_NODE=8
+FPN_POST_NMS_TOP_N_TRAIN=1000
+OPTIMIZER="NovoGrad"
+LR_SCHEDULE="COSINE"
+BETA1=0.9
+BETA2=0.25
+LS=0.1
+
+# cd /shared/mzanur/training_results_v0.7/NVIDIA/benchmarks/maskrcnn/implementations/pytorch
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#  -u -m bind_launch --master_addr=192.168.64.212 --nsockets_per_node=${NSOCKETS_PER_NODE} \
+# --ncores_per_socket=${NCORES_PER_SOCKET} --nproc_per_node=${NPROC_PER_NODE} \
+#
+for BETA2 in 0.2 0.22 0.25 0.27 0.3 0.35 0.4
+do
+/shared/herring_mzanur/bin/herringrun -n 32 -c /shared/herring_mzanur/ \
+ USE_HERRING_ALL_REDUCE=1 /shared/herring_mzanur/bin/python tools/train_mlperf.py \
+ --config-file 'configs/e2e_mask_rcnn_R_50_FPN_1x_giou_novo_ls.yaml' \
+ DTYPE 'float16' \
+ PATHS_CATALOG 'maskrcnn_benchmark/config/paths_catalog.py' \
+ DISABLE_REDUCED_LOGGING True \
+ INPUT.ADD_NOISE True \
+ SOLVER.BASE_LR ${BASE_LR} \
+ SOLVER.WEIGHT_DECAY ${WEIGHT_DECAY} \
+ SOLVER.MAX_ITER ${MAX_ITER} \
+ SOLVER.WARMUP_FACTOR ${WARMUP_FACTOR} \
+ SOLVER.WARMUP_ITERS ${WARMUP_ITERS} \
+ SOLVER.WEIGHT_DECAY_BIAS 0 \
+ SOLVER.WARMUP_METHOD linear \
+ SOLVER.IMS_PER_BATCH ${TRAIN_IMS_PER_BATCH} \
+ SOLVER.OPTIMIZER ${OPTIMIZER} \
+ SOLVER.BETA1 ${BETA1} \
+ SOLVER.BETA2 ${BETA2} \
+ MODEL.RPN.LS ${LS} \
+ MODEL.ROI_HEADS.LS ${LS} \
+ SOLVER.LR_SCHEDULE ${LR_SCHEDULE} \
+ TEST.IMS_PER_BATCH ${TEST_IMS_PER_BATCH} \
+ MODEL.RPN.FPN_POST_NMS_TOP_N_TRAIN ${FPN_POST_NMS_TOP_N_TRAIN} \
+ NHWC True | tee ${BASEDIR}/log_${BETA2}
+done
