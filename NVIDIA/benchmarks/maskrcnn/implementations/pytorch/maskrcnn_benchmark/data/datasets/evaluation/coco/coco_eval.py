herring integration fixes novograd loss scaling fix - integration with apex FP16Optimizer noise addition fix - disable for eval phase hyperparam adjustment herring and non/herring runs have similar convergence behavior - all runs converge in 16 epochs or less without CARL loss

NVIDIA/benchmarks/maskrcnn/implementations/pytorch/maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
@@ -7,7 +7,6 @@ import torch
 from collections import OrderedDict
 from tqdm import tqdm
 from multiprocessing import Pool
-import time
 
 from maskrcnn_benchmark.modeling.roi_heads.mask_head.inference import Masker
 from maskrcnn_benchmark.structures.bounding_box import BoxList
@@ -44,21 +43,18 @@ def remove_dup(l):
 
 def evaluate_coco(dataset, coco_results, iou_types, output_folder):
     results = COCOResults(*iou_types)
+    # logger.info("Evaluating predictions")
     print("Evaluating predictions")
     dataset.coco.createIndex(use_ext=True)
     for iou_type in iou_types:
-        res = evaluate_predictions_on_coco(
-                dataset.coco, coco_results[iou_type], iou_type
-                )
-        results.update(res)
-   #     with tempfile.NamedTemporaryFile() as f:
-   #         file_path = f.name
-   #         if output_folder:
-   #             file_path = os.path.join(output_folder, iou_type + ".json")
-   #         res = evaluate_predictions_on_coco(
-   #             dataset.coco, coco_results[iou_type], file_path, iou_type
-   #         )
-    #        results.update(res)
+        with tempfile.NamedTemporaryFile() as f:
+            file_path = f.name
+            if output_folder:
+                file_path = os.path.join(output_folder, iou_type + ".json")
+            res = evaluate_predictions_on_coco(
+                dataset.coco, coco_results[iou_type], file_path, iou_type
+            )
+            results.update(res)
     return results
 
 def do_coco_evaluation(
@@ -71,7 +67,7 @@ def do_coco_evaluation(
     expected_results_sigma_tol,
 ):
     logger = logging.getLogger("maskrcnn_benchmark.inference")
-    total_eval_start = time.time()
+
     # Different path here, fast parallel method not available, fall back to effectively the old
     # path.
     if box_only:
@@ -96,26 +92,16 @@ def do_coco_evaluation(
         return
     logger.info("Preparing results for COCO format")
     coco_results = {}
-    prepare_start = time.time()
     if "bbox" in iou_types:
         logger.info("Preparing bbox results")
-        bbox_start = time.time()
         coco_results["bbox"] = prepare_for_coco_detection(predictions, dataset)
-        bbox_end = time.time() - bbox_start
-        print("timing for bbox ", bbox_end)
     if "segm" in iou_types:
         logger.info("Preparing segm results")
-        segm_start = time.time()
         coco_results["segm"] = prepare_for_coco_segmentation(predictions, dataset)
-        segm_end = time.time()-segm_start
-        print("timing for segm ", segm_end)
     if 'keypoints' in iou_types:
         logger.info('Preparing keypoints results')
         coco_results['keypoints'] = prepare_for_coco_keypoint(predictions, dataset)
-    prepare_time = time.time() - prepare_start
-    print("bbox, segm, keypoints preparation ", prepare_time)
 
-    gather_start = time.time()
     # Gather all prepared predictions of each type from all ranks
     if "bbox" in iou_types:
         temp_bbox_list = all_gather(coco_results["bbox"])
@@ -123,8 +109,6 @@ def do_coco_evaluation(
         temp_segm_list = all_gather(coco_results["segm"])
     if "keypoints" in iou_types:
         temp_keypoints_list = all_gather(coco_results["keypoints"])
-    gather_time = time.time() - gather_start
-    print("All gather time: ", gather_time)
 
     # Only main process will call COCO
     if not is_main_process():
@@ -138,28 +122,22 @@ def do_coco_evaluation(
     if "keypoints" in iou_types:
         coco_results["keypoints"] = [i for j in temp_keypoints_list for i in j]
 
-    start_eval_time = time.time()
-    results = evaluate_coco(dataset, coco_results, iou_types, output_folder)
-    cocoeval_time = time.time() - start_eval_time
-    print("COCOeval time : ", cocoeval_time)
     # Submit to async evaluator
-   # get_evaluator().submit_task(get_tag(),
-    #                            evaluate_coco,
-    #                            dataset,
-    #                            coco_results,
-    #                            iou_types,
-    #                            output_folder)
+    get_evaluator().submit_task(get_tag(),
+                                evaluate_coco,
+                                dataset,
+                                coco_results,
+                                iou_types,
+                                output_folder)
     # Note: None of these are possible now
-   # logger.info(results)
-    check_expected_results(results, expected_results, expected_results_sigma_tol)
-    total_eval_end = time.time()-total_eval_start
-    print("Total eval : ", total_eval_end)
+    # logger.info(results)
+    # check_expected_results(results, expected_results, expected_results_sigma_tol)
     # if output_folder:
     #     torch.save(results, os.path.join(output_folder, "coco_results.pth"))
 
     # Note: results is now empty, the relevant future is held in the hidden
     # AsyncEvaluator object
-    return results, coco_results
+    return None, coco_results
 
 
 def prepare_for_coco_detection(predictions, dataset):
@@ -203,7 +181,6 @@ def prepare_for_coco_segmentation(predictions, dataset):
     masker = Masker(threshold=0.5, padding=1)
     # assert isinstance(dataset, COCODataset)
     coco_results = []
-
     for image_id, prediction in predictions.items():
         original_id = dataset.id_to_img_map[image_id]
         if len(prediction) == 0:
@@ -225,7 +202,7 @@ def prepare_for_coco_segmentation(predictions, dataset):
         labels = prediction.get_field("labels").tolist()
 
         # rles = prediction.get_field('mask')
-        
+
         rles = [
             mask_util.encode(np.array(mask[0, :, :, np.newaxis], order="F"))[0]
             for mask in masks
@@ -397,33 +374,25 @@ def evaluate_box_proposals(
 
 
 def evaluate_predictions_on_coco(
-    coco_gt, coco_results, iou_type="bbox"
+    coco_gt, coco_results, json_result_file, iou_type="bbox"
 ):
- #   import json
- #   print("coco results type: ", type(coco_results))
- #   print("coco results length: ", len(coco_results))
- #   json_start = time.time()
+    import json
+
     # Some run conditions can result in duplicate entries in the results.
     # This makes COCO unhappy and reduces the final accuracies -- remove such
     # duplicated results here before passing over to cocotools
- #   set_of_json = remove_dup([json.dumps(d) for d in coco_results])
- #   unique_list = [json.loads(s) for s in set_of_json]
+    set_of_json = remove_dup([json.dumps(d) for d in coco_results])
+    unique_list = [json.loads(s) for s in set_of_json]
 
-  #  with open(json_result_file, "w") as f:
-  #      json.dump(unique_list, f)
-
-  #  json_end = time.time() - json_start
-  #  print("json files dumping time", json_end)
+    with open(json_result_file, "w") as f:
+        json.dump(unique_list, f)
 
     from pycocotools.coco import COCO
     from pycocotools.cocoeval import COCOeval
 
- #   coco_dt = coco_gt.loadRes(str(json_result_file), use_ext=True) if coco_results else COCO()
- #   print(coco_dt)
- #   print(type(coco_dt))
-    # import numpy as np
-    print("pred lengthss ",len(coco_results))
-    coco_dt = coco_gt.loadRes(coco_results, use_ext=True)
+    coco_dt = coco_gt.loadRes(str(json_result_file), use_ext=True) if coco_results else COCO()
+
+    # coco_dt = coco_gt.loadRes(coco_results)
     coco_eval = COCOeval(coco_gt, coco_dt, iou_type, use_ext=True)
     coco_eval.evaluate()
     coco_eval.accumulate()

NVIDIA/benchmarks/maskrcnn/implementations/pytorch/maskrcnn_benchmark/engine/trainer.py
@@ -7,7 +7,6 @@ import time
 import torch
 
 import maskrcnn_benchmark.utils.comm as comm
-# from maskrcnn_benchmark.utils.comm import get_world_size, is_main_process, synchronize, reduce
 from maskrcnn_benchmark.utils.metric_logger import MetricLogger
 
 from apex import amp
@@ -35,6 +34,7 @@ def reduce_loss_dict(loss_dict):
             all_losses /= world_size
         reduced_losses = {k: v for k, v in zip(loss_names, all_losses)}
     return reduced_losses
+
 
 # Instead of zeroing, set parameter grads to None
 # Prevents extraneous copy as we're not accumulating
@@ -123,8 +123,6 @@ def do_train(
         # Otherwise apply loss scaling for mixed-precision recipe
         # with optimizer.scale_loss(losses) as scaled_losses:
         optimizer.backward(losses)
-        #AS: Introduce gradient clipping here
-        # torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
         optimizer.step()
         # set_grads_to_none(model)
         optimizer.zero_grad()

NVIDIA/benchmarks/maskrcnn/implementations/pytorch/maskrcnn_benchmark/modeling/rpn/inference.py
@@ -219,7 +219,7 @@ class RPNPostProcessor(torch.nn.Module):
           for j in range(0, num_images):
             end_idx = start_idx + num_boxes_batched[ptr]
             keep_per_box = keep_inds_batched[start_idx:end_idx]
-            inds_per_box = keep_per_box.nonzero().squeeze(1)
+            inds_per_box = keep_per_box.nonzero(as_tuple=False).squeeze(1)
             keep_size = min(inds_per_box.size(0), self.post_nms_top_n)
             inds_per_box = inds_per_box[:keep_size]
             per_level_boxlist.append(sampled_boxes[i][j][inds_per_box])
@@ -331,7 +331,7 @@ class RPNPostProcessor(torch.nn.Module):
         objectness_gen = objectness_gen.reshape(-1)
         objectness_kept = objectness_gen
         # post NMS topN
-        num_keeps = keep.reshape(-1).nonzero().squeeze(1).size(0)
+        num_keeps = keep.reshape(-1).nonzero(as_tuple=False).squeeze(1).size(0)
 
         _, inds_sorted = torch.topk(objectness_kept, min(self.fpn_post_nms_top_n,num_keeps), dim=0, sorted=False)
         inds_sorted, _ = inds_sorted.sort()

NVIDIA/benchmarks/maskrcnn/implementations/pytorch/maskrcnn_benchmark/structures/bounding_box.py
@@ -220,7 +220,7 @@ class BoxList(object):
     def __getitem__(self, item):
         item_type = item.type()
         if item_type=='torch.ByteTensor' or item_type=='torch.cuda.ByteTensor':
-          item = torch.nonzero(item).squeeze(1)
+          item = torch.nonzero(item, as_tuple=False).squeeze(1)
         item = item.to(device=self.bbox.device)
         bbox = BoxList(self.bbox.index_select(0,item), self.size, self.mode)
         for k, v in self.extra_fields.items():

NVIDIA/benchmarks/maskrcnn/implementations/pytorch/tools/train_mlperf.py
@@ -172,15 +172,15 @@ def train(cfg, local_rank, distributed, random_number_generator=None):
     device = torch.device(cfg.MODEL.DEVICE)
     model.to(device)
 
+    optimizer = make_optimizer(cfg, model)
     # Initialize mixed-precision training
     is_fp16 = (cfg.DTYPE == "float16")
     if is_fp16:
         # convert model to FP16
         model.half()
 
-    optimizer = make_optimizer(cfg, model)
     # Optimizer logging
-    log_event(key=constants.OPT_NAME, value="sgd_with_momentum")
+    log_event(key=constants.OPT_NAME, value=cfg.SOLVER.OPTIMIZER)
     log_event(key=constants.OPT_BASE_LR, value=cfg.SOLVER.BASE_LR)
     log_event(key=constants.OPT_LR_WARMUP_STEPS, value=cfg.SOLVER.WARMUP_ITERS)
     log_event(key=constants.OPT_LR_WARMUP_FACTOR, value=cfg.SOLVER.WARMUP_FACTOR)
@@ -198,7 +198,7 @@ def train(cfg, local_rank, distributed, random_number_generator=None):
         if not use_herring:
             model = DDP(model, delay_allreduce=True)
         else:
-            model = DDP(model, device_ids=[get_local_rank()], broadcast_buffers=False)
+            model = DDP(model, device_ids=[get_local_rank()], broadcast_buffers=False, bucket_cap_mb=25)
 
     arguments = {}
     arguments["iteration"] = 0
@@ -215,7 +215,8 @@ def train(cfg, local_rank, distributed, random_number_generator=None):
     arguments.update(extra_checkpoint_data)
     
     if is_fp16:
-        optimizer = FP16_Optimizer(optimizer, dynamic_loss_scale=True)
+        import apex
+        optimizer = apex.fp16_utils.fp16_optimizer.FP16_Optimizer(optimizer, dynamic_loss_scale=True)
 
     log_end(key=constants.INIT_STOP)
     barrier()